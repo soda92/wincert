4cd5f3450af09af545eb0fa7a491837f30f97882
Maple <wang-jiaxun@outlook.com>
2024-06-04 09:43:51 +0800

    fin

---
+++ get-cert.ps1
@@ -0,0 +1,23 @@
+# Get the signature
+$attestedDoc = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri http://169.254.169.254/metadata/attested/document?api-version=2020-09-01
+# Decode the signature
+$signature = [System.Convert]::FromBase64String($attestedDoc.signature)
+
+# Get certificate chain
+$cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]($signature)
+$chain = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Chain
+$chain.Build($cert)
+# Print the Subject of each certificate in the chain
+foreach($element in $chain.ChainElements)
+{
+    Write-Host $element.Certificate.Subject
+}
+
+# Get the content of the signed document
+Add-Type -AssemblyName System.Security
+$signedCms = New-Object -TypeName System.Security.Cryptography.Pkcs.SignedCms
+$signedCms.Decode($signature);
+$content = [System.Text.Encoding]::UTF8.GetString($signedCms.ContentInfo.Content)
+Write-Host "Attested data: " $content
+$json = $content | ConvertFrom-Json
+# Do additional validation here
---
+++ install-cert.ps1
@@ -0,0 +1,14 @@
+﻿$certs = Get-ChildItem '*.crt'
+$storeLocation = "Cert:\LocalMachine\CA"
+
+for($i=0; $i -ne $certs.Count; $i++) {
+    $cert = $certs[$i].Name
+    echo $cert
+    try {
+        Import-Certificate -FilePath $cert -CertStoreLocation $storeLocation
+        Write-Output "Certificate imported successfully."
+    }
+    catch {
+        Write-Error "Error importing certificate: $cert"
+    }
+}
