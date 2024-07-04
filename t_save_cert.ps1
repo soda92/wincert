c5586a9f563cd62f9dac728d95da64810c5ee140
Maple <wang-jiaxun@outlook.com>
2024-07-04 16:08:09 +0800

    Update t_save_cert.ps1

--- t_save_cert.ps1
+++ t_save_cert.ps1
@@ -4,4 +4,5 @@ $WebSession.Proxy=$Proxy
 $attestedDoc = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri http://169.254.169.254/metadata/attested/document?api-version=2020-09-01 -WebSession $WebSession
 $signature = [System.Convert]::FromBase64String($attestedDoc.signature)
 $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]($signature)
-[System.IO.File]::WriteAllBytes("C:\IMDS.cer", $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
+$cwd = (Get-Location).Path
+[System.IO.File]::WriteAllBytes("$cwd\imds.crt", $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
